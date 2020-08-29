55e89d333472987613eb1b415a0081364e4bde45
Aaron B Johnson <johnsoaab@gmail.com>
2020-08-29 14:32:51 -0700

    Add watcher to build script

--- build.ps1
+++ build.ps1
@@ -1,5 +1,38 @@
-tsc
-copy -Path "./node_modules/dropbox/dist/Dropbox-sdk.min.js" -Destination "./publish/Dropbox-sdk.js"
-copy -Path "./node_modules/dexie/dist/dexie.js" -Destination "./publish/dexie.js"
-copy -Path "./popup.html" -Destination "./publish/popup.html"
-copy -Path "./manifest.json" -Destination "./publish/manifest.json"
+Function Build {
+    tsc
+    copy -Path "./node_modules/dropbox/dist/Dropbox-sdk.min.js" -Destination "./publish/Dropbox-sdk.js"
+    copy -Path "./node_modules/dexie/dist/dexie.js" -Destination "./publish/dexie.js"
+    copy -Path "./popup.html" -Destination "./publish/popup.html"
+    copy -Path "./manifest.json" -Destination "./publish/manifest.json"
+}
+
+Function Watch {
+    $global:FileChanged = $false
+    $folder = (pwd).Path
+    $watcher = [IO.FileSystemWatcher]::new($folder)
+    $watcher.IncludeSubdirectories = $false
+    $watcher.EnableRaisingEvents = $true
+
+    Register-ObjectEvent $watcher "Changed" -Action {$global:FileChanged = $true} > $null
+
+    while ($true) {
+        while ($global:FileChanged -eq $false) {
+            Start-Sleep -Milliseconds 100
+        }
+
+        Write-Host "Building detected changes.. " -NoNewLine
+        Build
+        Write-Host "Done."
+        $global:FileChanged = $false
+    }
+}
+
+if ($args.Length -eq 0) {
+    Build
+} elseif ($args[0] -eq "build") {
+    Build
+}elseif ($args[0] -eq "watch") {
+    Watch
+} else {
+    echo "Unrecognized arguments: $args"
+}
