ec75cc10009a14e897b00b04b5c6ebfe1d9b5450
Aaron B Johnson <johnsoaab@gmail.com>
2020-09-03 21:28:12 -0700

    build script updates: use tsc -w on watch, copy types to @types

--- build.js
+++ build.js
@@ -2,55 +2,112 @@ const chokidar = require("chokidar");
 const process = require("process");
 const { spawn } = require("child_process");
 const fs = require("fs");
+const fsp = fs.promises;
+const path = require("path");
+
+function getFiles(atPath) {
+  let current = null;
+  let paths = [atPath];
+
+  async function next() {
+    let statPath = paths.shift();
+    if (!statPath) {
+      current = null;
+      return false;
+    }
+
+    let stat = await fsp.stat(statPath);
+    if (stat.isFile()) {
+      current = statPath;
+      return true;
+    } else if (stat.isDirectory()) {
+      const subpaths = (await fsp.readdir(statPath)).map((p) =>
+        path.join(statPath, p)
+      );
+      Array.prototype.push.apply(paths, subpaths);
+      return next();
+    }
+
+    throw `Not a directory or a file: ${statPath}`;
+  }
+
+  return { current: () => current, next: next };
+}
+
+async function copyToPublish() {
+  await fsp.copyFile(
+    "./node_modules/dropbox/dist/Dropbox-sdk.min.js",
+    "./publish/Dropbox-sdk.js"
+  );
+
+  await fsp.copyFile(
+    "./node_modules/dexie/dist/dexie.js",
+    "./publish/dexie.js"
+  );
+
+  await fsp.copyFile(
+    "./node_modules/xlsx/dist/xlsx.full.min.js",
+    "./publish/xlsx.full.min.js"
+  );
+
+  await fsp.copyFile("./popup.html", "./publish/popup.html");
+  await fsp.copyFile("./manifest.json", "./publish/manifest.json");
+}
+
+// Copy */types/index.d.ts files to @types/*/index.d.ts to satisfy TypeScript
+async function fixTscTypes() {
+  const files = getFiles("./node_modules");
+  while (await files.next()) {
+    const file = files.current();
+    const dtsMatch = file.match(
+      /node_modules[\\/](.*)[\\/]types[\\/]index\.d\.ts$/
+    );
+    if (dtsMatch && dtsMatch.length > 0) {
+      const package = dtsMatch[1];
+      if (package === "chokidar") continue;
+      const packagePath = `./node_modules/@types/${package}`;
+      if (!(await fsp.stat(packagePath)).isDirectory(packagePath)) {
+        await fsp.mkdir(packagePath);
+      }
+      await fsp.copyFile(file, path.join(packagePath, "index.d.ts"));
+    }
+  }
+}
+
+async function runTsc() {
+  const tsc = spawn("tsc", [], { shell: true, stdio: "inherit" });
+  return new Promise((resolve) => {
+    tsc.on("close", (code) => resolve(code));
+  });
+}
 
 async function build() {
   process.stdout.write(`${new Date().toISOString()} Building.. \n`);
-
-  const tsc = spawn("tsc", [], { shell: true, stdio: "inherit" });
-  const b = new Promise((resolve) => {
-    tsc.on("close", (code) => resolve(code));
-  });
-
-  const code = await b;
-  function onErr(err) {
-    if (err) throw err;
-  }
-
-  fs.copyFile(
-    "./node_modules/dropbox/dist/Dropbox-sdk.min.js",
-    "./publish/Dropbox-sdk.js",
-    onErr
-  );
-
-  fs.copyFile(
-    "./node_modules/dexie/dist/dexie.js",
-    "./publish/dexie.js",
-    onErr
-  );
-
-  fs.copyFile(
-    "./node_modules/xlsx/dist/xlsx.full.min.js",
-    "./publish/xlsx.full.min.js",
-    onErr
-  );
-
-  fs.copyFile("./popup.html", "./publish/popup.html", onErr);
-  fs.copyFile("./manifest.json", "./publish/manifest.json", onErr);
-
-  if (code === 0) process.stdout.write("Finished.");
+  await copyToPublish();
+  await fixTscTypes();
+  let code = await runTsc();
+  if (code === 0) process.stdout.write(`${new Date().toISOString()} Done.`);
 }
 
 function watch() {
+  const tsc = spawn("tsc", ["-w"], { shell: true, stdio: "inherit" });
   let changes = [];
   chokidar
-    .watch(".", { depth: 0, awaitWriteFinish: true })
+    .watch(".", {
+      depth: 0,
+      awaitWriteFinish: true,
+      ignored: /(.*).js$|(.*).ts$/,
+    })
     .on("change", async (event, path) => {
       changes.push(new Date().toISOString());
       if (changes.length > 1) return;
 
       let last = changes.pop();
       while (last) {
-        await build();
+        process.stdout.write(
+          `${new Date().toISOString()} Copying file changes.\n`
+        );
+        await copyToPublish();
         const latest = changes.pop();
         if (latest && latest > last) last = latest;
         else break;
